00c5c4ee1ab862b5c49c1e0f007bff6cfff6ff95
Mishkaboom <ogunkaracan8@gmail.com>
2016-12-05 21:20:11 +0200

    Add files via upload

---
+++ Basic Calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int a;
+	int b;
+	int sum ;
+	
+	
+	cout << "Please enter a number :" << endl;
+	cin >> a ; 
+	
+	cout << "Please enter a 2nd number : " << endl;
+    cin >> b ; 
+    
+    sum = a + b;
+    cout << sum << endl;
+    
+    return 0;
+}
+
